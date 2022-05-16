99ab858c27675f04bb3f501fc242ffc6f9412423
gadaharsh <gada.hp@somaiya.edu>
2022-05-16 13:56:51 +0530

    added all text items

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,13 +3,12 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   useWindowDimensions,
 } from "react-native";
 import React from "react";
 import icon from "../assets/Wavy_Lst-01_Single-11.jpg";
-import appName from "../assets/1.jpg";
-
-// const appName = { uri :" "};
+import appName from "../assets/appName.png";
 
 const HomeScreen = () => {
   const { height } = useWindowDimensions();
@@ -21,14 +20,18 @@ const HomeScreen = () => {
           resizeMode="cover"
           style={[styles.icon, { height: height * 0.5 }]}
         >
-            <Image source={appName}  />
+          <Image source={appName} />
         </ImageBackground>
       </View>
 
       <View style={styles.lowerSection}>
-        <Text>The personality first connecting app</Text>
-        <Text>If you were a fruit, then you would be a fine-apple</Text>
-        <Text>Already a user? Try Signing In</Text>
+        <Text style={styles.heading1}>
+          The personality first connecting app
+        </Text>
+        <Text style={styles.heading2}>
+          If you were a fruit, then you would be a fine-apple
+        </Text>
+        <Text style={styles.heading3}>Already a user? Try Signing In</Text>
       </View>
     </View>
   );
@@ -42,6 +45,8 @@ const styles = StyleSheet.create({
     // backgroundColor: '#fff',
     // alignItems: "center",
     // justifyContent: "center",
+    // display: 'flex',
+    //   flexDirection: 'column',
   },
   image: {
     flex: 1,
@@ -49,19 +54,43 @@ const styles = StyleSheet.create({
     height: "50%",
   },
   icon: {
-    // margin: "15%",
+    // marginVertical: "2%",
     width: "100%",
     // height: "70%",
     maxHeight: 300,
-    maxWidth: 300,
+    maxWidth: 400,
     resizeMode: "contain",
     // borderRadius: 2,
     // overflow: "hidden",
   },
-  upperSection:{
-
+  upperSection: {
+    // backgroundColor: '#E8F2F6',
+    flex: 1,
+    borderBottomColor: "#E8F2F6",
+    borderBottomWidth: 3,
   },
-  lowerSection:{
+  lowerSection: {
+    flex: 1,
+  },
+  heading1: {
+    fontSize: 36,
+    // fontWeight: "bold",
+    marginHorizontal: "5%",
 
-  }
+    paddingVertical: "5%",
+    justifyContent: "center",
+  },
+  heading2: {
+    fontSize: 14,
+    // textAlign: "center",
+    paddingVertical: "2%",
+    marginHorizontal: "5%",
+  },
+  heading3: {
+    fontSize: 14,
+    // textAlign: "center",
+    paddingVertical: "2%",
+    marginHorizontal: "5%",
+    // paddingHorizontal: "5%",
+  },
 });
